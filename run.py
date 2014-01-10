de33b419a9578ed4016d9b349581c54489bb3037
shaofang <shaofang.zhang@gmail.com>
2014-01-10 17:47:51 +0800

    update browser and game

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import sys
 loop = 1
 if sys.argv[1] == '--loop':
     loop = sys.argv[2]
-inputs = ['', '--with-plan-loader', '--with-device-config', '--with-local-report','--verbosity=2']
+inputs = ['', '--with-plan-loader', '--with-device-config', '--with-file-output','--verbosity=2']
 for i in range(int(loop)):
     nose.run(argv=inputs)
 
